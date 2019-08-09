cc7387d7c0b888af4aa04b5a44178288d9fb77ea
常帅 <changshuai@bonc.com.cn>
2019-08-09 17:23:43 +0800

    first commit

--- sort/QuickSort.go
+++ sort/QuickSort.go
@@ -3,11 +3,13 @@ package main
 func QuickSort(arr *[]int, p int, r int) {
 	if p < r {
 		q := Partition(arr, p, r)
-		QuickSort()
+		QuickSort(arr, p, q-1)
+		QuickSort(arr, q+1, r)
 	}
 }
 
-func Partition() {
+func Partition(arr *[]int, p int, r int) int {
+	x := *arr[r]
 
 }
 

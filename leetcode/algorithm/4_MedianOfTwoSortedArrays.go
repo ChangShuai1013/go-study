7f67eb29488037a06a5575b92724ce65888046ba
常帅 <changshuai@bonc.com.cn>
2019-08-16 17:16:09 +0800

    1.完成两已排序数组的中位数问题

--- leetcode/algorithm/4_MedianOfTwoSortedArrays.go
+++ leetcode/algorithm/4_MedianOfTwoSortedArrays.go
@@ -1,5 +1,49 @@
 package main
 
-func main() {
+import "math"
 
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	m := len(nums1)
+	n := len(nums2)
+	if m > n {
+		temp := nums1
+		nums1 = nums2
+		nums2 = temp
+		tmp := m
+		m = n
+		n = tmp
+	}
+	var iMin, iMax int = 0, m
+	halfLen := (m + n + 1) / 2
+	for iMin <= iMax {
+		i := (iMin + iMax) / 2
+		j := halfLen - i
+		if i < iMax && nums2[j-1] > nums1[i] {
+			iMin = i + 1
+		} else if i > iMin && nums1[i-1] > nums2[j] {
+			iMax = i - 1
+		} else {
+			maxLeft := 0.0
+			if i == 0 {
+				maxLeft = float64(nums2[j-1])
+			} else if j == 0 {
+				maxLeft = float64(nums1[i-1])
+			} else {
+				maxLeft = math.Max(float64(nums1[i-1]), float64(nums2[j-1]))
+			}
+			if (m+n)%2 == 1 {
+				return maxLeft
+			}
+			minRight := 0.0
+			if i == m {
+				minRight = float64(nums2[j])
+			} else if j == n {
+				minRight = float64(nums1[i])
+			} else {
+				minRight = math.Min(float64(nums1[i]), float64(nums2[j]))
+			}
+			return (maxLeft + minRight) / 2.0
+		}
+	}
+	return 0.0
 }
